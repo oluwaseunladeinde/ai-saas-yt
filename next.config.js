fc2c3fa7bb5aa0b300b45e7eb97141527e79a621
oluwaseunladeinde <oluwaseun.ladeinde@yahoo.com>
2024-01-09 14:22:32 +0100

    add image ai generation

--- next.config.js
+++ next.config.js
@@ -1,4 +1,21 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+    images: {
+        remotePatterns: [
+            {
+                protocol: "https",
+                hostname: "googleusercontent.com",
+            },
+            {
+                protocol: "https",
+                hostname: "oaidalleapiprodscus.blob.core.windows.net"
+            },
+            {
+                protocol: "https",
+                hostname: "cdn.openai.com"
+            }
+        ],
+    },
+}
 
 module.exports = nextConfig
